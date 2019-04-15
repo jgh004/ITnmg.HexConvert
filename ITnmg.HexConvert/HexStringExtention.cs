db0fcd061b8e6eb8d0c630c0d187a3c70c3bb7da
Kim <jgh004@163.com>
2019-04-15 22:26:33 +0800

    增加16进制字符串分隔符

--- ITnmg.HexConvert/HexStringExtention.cs
+++ ITnmg.HexConvert/HexStringExtention.cs
@@ -1,33 +1,34 @@
 ﻿using System;
 using System.Text.RegularExpressions;
 
-namespace ITnmg.HexConvert
+namespace System
 {
     /// <summary>
     /// Hexadecimal string and byte arrays convert to each other
     /// </summary>
-    public static class HexConvert
+    public static class HexStringExtention
     {
         /// <summary>
+        /// 将字节数组转换为16进制字符串。
 		/// Convert the byte arrays to hexadecimal string
 		/// </summary>
-		/// <param name="bytes">byte arrays</param>
-		/// <exception cref="ArgumentNullException">When the bytes is null</exception>
+		/// <param name="bytes">字节数组。byte arrays</param>
+		/// <exception cref="ArgumentNullException">如果字节数组为 null,将抛出异常。When the bytes is null</exception>
 		/// <returns>Hexadecimal string</returns>
-		public static string ToHexString( this byte[] bytes )
+		public static string ToHexString( this byte[] bytes, string separator = "" )
         {
             if ( bytes == null )
             {
                 throw new ArgumentNullException( nameof( bytes ) );
             }
 
-            return BitConverter.ToString( bytes ).Replace( "-", "" );
+            return BitConverter.ToString( bytes ).Replace( "-", separator );
         }
 
         /// <summary>
-        /// Verify if that string is a hexadecimal string
+        /// 验证字符串是否为16进制数字表示。Verify if that string is a hexadecimal string
         /// </summary>
-        /// <param name="hexStr">A string</param>
+        /// <param name="hexStr">要验证的字符串。A string</param>
         /// <returns>True or False</returns>
         public static bool IsHexString( this string hexStr )
         {
@@ -50,11 +51,11 @@ namespace ITnmg.HexConvert
         }
 
         /// <summary>
-        /// Convert the hexadecimal string to byte arrays
+        /// 将16进制字符串转换为字节数组。Convert the hexadecimal string to byte arrays
         /// </summary>
-        /// <param name="hexStr">Hexadecimal string</param>
-        /// <exception cref="ArgumentNullException">When the hexStr is null</exception>
-        /// <exception cref="ArgumentException">When hexStr.Length is odd, or hexStr without 0-9, a-f, A-F</exception>
+        /// <param name="hexStr">16进制字符串，长度必须为偶数位。Hexadecimal string</param>
+        /// <exception cref="ArgumentNullException">如果字符串为 null，将抛出异常。When the hexStr is null</exception>
+        /// <exception cref="ArgumentException">如果字符串长度为奇数，或字符串中包含非 0-9, a-f, A-F 的字符，将抛出异常。When hexStr.Length is odd, or hexStr without 0-9, a-f, A-F</exception>
         /// <returns>null or byte arrays</returns>
         public static byte[] ToBytes( string hexStr )
         {
